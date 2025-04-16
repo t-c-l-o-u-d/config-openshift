4194445a9a88b54472fa7ce54a44920b8d5a76ef
tcloud <github@varmack.com>
2025-04-16 20:57:23 +0000

    fix function name

--- openshift-cnv-operator/0-openshift-cnv-setup.bash
+++ openshift-cnv-operator/0-openshift-cnv-setup.bash
@@ -6,8 +6,8 @@ oc create -f ./2-openshift-cnv-operatorgroup.yaml
 
 oc apply -f ./3-openshift-cnv-subscription.yaml
 
-source ~/.config/bash/functions && show_wait_timer 120
+source ~/.config/bash/functions && _show_wait_timer 120
 
 oc apply -f ./4-openshift-cnv-hyperconverged.yaml
 
-source ~/.config/bash/functions && show_wait_timer 300
+source ~/.config/bash/functions && _show_wait_timer 300
