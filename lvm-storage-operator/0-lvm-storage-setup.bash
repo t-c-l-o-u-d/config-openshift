2f43ebcbba31e692e904b4c9689dfba5ea5017c8
tcloud <github@varmack.com>
2025-04-16 20:55:29 +0000

    fix path to functions

--- lvm-storage-operator/0-lvm-storage-setup.bash
+++ lvm-storage-operator/0-lvm-storage-setup.bash
@@ -6,8 +6,8 @@ oc apply -f ./2-lvm-storage-operatorgroup.yaml
 
 oc apply -f ./3-lvm-storage-subscription.yaml
 
-source ~/.bashrc-functions && show_wait_timer 60
+source ~/.config/bash/functions && _show_wait_timer 60
 
 oc apply -f ./4-lvm-storage-lvmcluster.yaml
 
-source ~/.bashrc-functions && show_wait_timer 60
+source ~/.config/bash/functions && _show_wait_timer 60
