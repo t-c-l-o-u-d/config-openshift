b7381429c01a4e51defb730944fe08febfb3facc
tcloud <github@varmack.com>
2025-04-16 20:54:57 +0000

    increase wait times

--- gpu-passthrough/0-gpu-passthrough-setup.bash
+++ gpu-passthrough/0-gpu-passthrough-setup.bash
@@ -2,16 +2,25 @@
 
 FILE_LIST=$(find . -type f -name "*.bu")
 
-for FILE in ${FILE_LIST};
-do
+for FILE in ${FILE_LIST}; do
     FILE_NAME=$(echo ${FILE} | sed --expression='s/\.bu//' --expression='s/^\.\///')
     butane ${FILE} -o ./${FILE_NAME}.yaml
 done
 
 oc apply -f ./100-master-blacklist-amdgpu.yaml
 
+# time to apply mc and shutdown: ~7m
+# time to boot: ~5m
+source ~/.config/bash/functions && _show_wait_timer 720
+
 oc apply -f ./100-master-kernel-arg-disable-efi-framebuffer.yaml
+
+source ~/.config/bash/functions && _show_wait_timer 720
 
 oc apply -f ./100-master-kernel-arg-enable-iommu.yaml
 
+source ~/.config/bash/functions && _show_wait_timer 720
+
 oc apply -f ./100-master-vfio-pci.yaml
+
+source ~/.config/bash/functions && _show_wait_timer 720
